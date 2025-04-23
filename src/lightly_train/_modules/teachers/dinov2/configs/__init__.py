f03a9b5f9157f92fd3cb0da8c356697cd7882190
Thomas <thomas@lightly.ai>
2025-04-23 10:12:53 +0200

    Add correct header to DINOv2 files

--- src/lightly_train/_modules/teachers/dinov2/configs/__init__.py
+++ src/lightly_train/_modules/teachers/dinov2/configs/__init__.py
@@ -1,10 +1,8 @@
 #
-# Copyright (c) Lightly AG and affiliates.
-# All rights reserved.
+# Copyright (c) Meta Platforms, Inc. and affiliates.
 #
-# This source code is licensed under the license found in the
-# LICENSE file in the root directory of this source tree.
-#
+# This source code is licensed under the Apache License, Version 2.0
+# found in the LICENSE file in the root directory of this source tree.#
 
 import pathlib
 
--- src/lightly_train/_modules/teachers/dinov2/layers/layer_scale.py
+++ src/lightly_train/_modules/teachers/dinov2/layers/layer_scale.py
@@ -1,10 +1,8 @@
 #
-# Copyright (c) Lightly AG and affiliates.
-# All rights reserved.
+# Copyright (c) Meta Platforms, Inc. and affiliates.
 #
-# This source code is licensed under the license found in the
-# LICENSE file in the root directory of this source tree.
-#
+# This source code is licensed under the Apache License, Version 2.0
+# found in the LICENSE file in the root directory of this source tree.#
 
 # Modified from: https://github.com/huggingface/pytorch-image-models/blob/main/timm/models/vision_transformer.py#L103-L110
 
