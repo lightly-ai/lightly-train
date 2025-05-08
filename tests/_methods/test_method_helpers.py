a159601262d17fd78ca3656f48c4d390c160c536
Thomas <thomas@lightly.ai>
2025-05-08 15:03:42 +0200

    Test the method helper

--- tests/_methods/test_method_helpers.py
+++ tests/_methods/test_method_helpers.py
@@ -13,6 +13,7 @@ from lightly_train._methods import method_helpers
 from lightly_train._methods.densecl.densecl import DenseCL
 from lightly_train._methods.dino.dino import DINO
 from lightly_train._methods.distillation.distillation import Distillation
+from lightly_train._methods.distillationv2.distillationv2 import DistillationV2
 from lightly_train._methods.method import Method
 from lightly_train._methods.simclr.simclr import SimCLR
 
@@ -25,7 +26,9 @@ from .. import helpers
         ("densecl", DenseCL),
         ("dino", DINO),
         ("simclr", SimCLR),
-        ("distillation", Distillation),
+        ("distillation", DistillationV2),
+        ("distillationv1", Distillation),
+        ("distillationv2", DistillationV2),
         (helpers.get_method(), SimCLR),
     ],
 )
@@ -38,6 +41,7 @@ def test_list_methods_private() -> None:
         "densecl",
         "dino",
         "distillation",
+        "distillationv2",
         "simclr",
     ]
 
@@ -46,5 +50,6 @@ def test_list_methods_public() -> None:
     assert method_helpers.list_methods() == [
         "dino",
         "distillation",
+        "distillationv2",
         "simclr",
     ]
