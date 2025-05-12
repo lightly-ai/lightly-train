980eadff7dcf9871bba5962c253120abfce486a1
yutong <yutong@lightly.ai>
2025-05-12 17:33:37 +0200

    finish test

--- tests/_methods/dinov2/test_dinov2_loss.py
+++ tests/_methods/dinov2/test_dinov2_loss.py
@@ -105,29 +105,30 @@ class TestDINOLoss:
 
     def test_forward(
         self,
-        batch_size=4,
         out_dim=2,
         teacher_temp=0.04,
         student_temp=0.1,
         center_momentum=0.9,
     ) -> None:
-        """Test that the forward method returns a non-negative scalar"""
+        """Test that the forward method returns correct values"""
 
         dino_loss = DINOLoss(
             out_dim=out_dim, student_temp=student_temp, center_momentum=center_momentum
         )
 
-        teacher_output = torch.randn(batch_size, out_dim)
+        teacher_output = torch.tensor([[0.1, 0.2], [0.3, 0.4], [0.5, 0.6]])
+        student_output = [
+            torch.tensor([[0.7, 0.8], [0.9, 1.0], [1.1, 1.2]]) for _ in range(2)
+        ]
+
         teacher_softmaxed = dino_loss.softmax_center_teacher(
             teacher_output, teacher_temp=teacher_temp
         )
         dino_loss.update_center(teacher_output)
-        
-        student_output = [torch.randn(batch_size, out_dim) for _ in range(2)]
-        loss = dino_loss(student_output, [teacher_softmaxed, teacher_softmaxed])
 
-        # loss should be non-negative scalar
-        assert loss.ndim == 0 and loss.item() >= 0
+        loss = dino_loss.forward(student_output, [teacher_softmaxed, teacher_softmaxed])
+
+        assert loss == pytest.approx(1.5565, rel=0.0001)
 
 
 @pytest.mark.usefixtures("no_dist")
@@ -145,7 +146,9 @@ class TestIBotPatchLoss:
         """
 
         ibot_loss = iBOTPatchLoss(
-            patch_out_dim=patch_out_dim, student_temp=student_temp, center_momentum=center_momentum
+            patch_out_dim=patch_out_dim,
+            student_temp=student_temp,
+            center_momentum=center_momentum,
         )
 
         teacher_output = torch.randn(batch_size, patch_out_dim)
@@ -171,13 +174,18 @@ class TestIBotPatchLoss:
         """
 
         ibot_loss = iBOTPatchLoss(
-            patch_out_dim=patch_out_dim, student_temp=student_temp, center_momentum=center_momentum
+            patch_out_dim=patch_out_dim,
+            student_temp=student_temp,
+            center_momentum=center_momentum,
         )
 
         teacher_output = torch.zeros(batch_size, patch_out_dim)
         n_masked_patches_tensor = torch.randint(high=batch_size, size=(1,))
         Q = ibot_loss.sinkhorn_knopp_teacher(
-            teacher_output, teacher_temp=teacher_temp, n_masked_patches_tensor=n_masked_patches_tensor, n_iterations=n_iterations
+            teacher_output,
+            teacher_temp=teacher_temp,
+            n_masked_patches_tensor=n_masked_patches_tensor,
+            n_iterations=n_iterations,
         )
 
         # Q shape = [B, K]
@@ -185,12 +193,14 @@ class TestIBotPatchLoss:
 
         # column sums ≈ B/K
         col_sums = Q.sum(dim=0)
-        assert torch.allclose(col_sums, torch.ones(patch_out_dim) * batch_size / patch_out_dim)
+        assert torch.allclose(
+            col_sums, torch.ones(patch_out_dim) * batch_size / patch_out_dim
+        )
 
         # row sums ≈ 1
         row_sums = Q.sum(dim=1)
         assert torch.allclose(row_sums, torch.ones(batch_size))
-        
+
     def test_update_center_momentum(
         self,
         batch_size=4,
@@ -204,7 +214,9 @@ class TestIBotPatchLoss:
         """
 
         ibot_loss = iBOTPatchLoss(
-            patch_out_dim=patch_out_dim, student_temp=student_temp, center_momentum=center_momentum
+            patch_out_dim=patch_out_dim,
+            student_temp=student_temp,
+            center_momentum=center_momentum,
         )
 
         # call update & apply on a known tensor
@@ -222,39 +234,33 @@ class TestIBotPatchLoss:
         student_temp=0.2,
         center_momentum=0.9,
     ) -> None:
+        """Test that the forward method returns correct values"""
+
         ibot_loss = iBOTPatchLoss(
-            patch_out_dim=out_dim, 
-            student_temp=student_temp, 
-            center_momentum=center_momentum
+            patch_out_dim=out_dim,
+            student_temp=student_temp,
+            center_momentum=center_momentum,
         )
-        
+
         masked_teacher_output = torch.tensor([[0.1, 0.2], [0.3, 0.4], [0.5, 0.6]])
         masked_student_output = torch.tensor([[0.7, 0.8], [0.9, 1.0], [1.1, 1.2]])
+
         mask = torch.tensor(
             [
-                [[True, False], [True, False]],
-                [[False, False], [False, True]],
-                [[False, False], [False, False]],
+                [True, False, True, False],
+                [False, False, False, True],
+                [False, False, False, False],
             ]
         )
-        masks_weight = torch.tensor(
-            [0.5, 1, 0]
-        )
-        n_masked_patches=masked_teacher_output.shape[0]
-        
+
         masked_teacher_softmaxed = ibot_loss.softmax_center_teacher(
             masked_teacher_output.unsqueeze(0), teacher_temp=teacher_temp
         )
         ibot_loss.update_center(masked_teacher_output.unsqueeze(0))
-        
+
         loss = ibot_loss.forward_masked(
             teacher_patch_tokens_masked=masked_teacher_softmaxed,
             student_patch_tokens_masked=masked_student_output,
             student_masks_flat=mask,
-            n_masked_patches=n_masked_patches,
-            masks_weight=masks_weight,
         )
         assert loss == pytest.approx(0.4057, rel=0.0001)
-        
-        expected_center = 0.1 * masked_teacher_output.mean(0)
-        assert torch.all(torch.isclose(ibot_loss.center.value, expected_center))
