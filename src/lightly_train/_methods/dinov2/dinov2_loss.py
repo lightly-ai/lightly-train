f4ab4ebdf9055cb8217fa4960f7a7d33764cd61d
yutong <yutong@lightly.ai>
2025-05-12 10:03:43 +0200

    fix ibot loss

--- src/lightly_train/_methods/dinov2/dinov2_loss.py
+++ src/lightly_train/_methods/dinov2/dinov2_loss.py
@@ -170,7 +170,8 @@ class iBOTPatchLoss(nn.Module):
         ).t()  # Q is K-by-B for consistency with notations from our paper
         # B = Q.shape[1] * world_size # number of samples to assign
         B = n_masked_patches_tensor
-        dist.all_reduce(B)
+        if dist.is_initialized():
+            dist.all_reduce(B)
         K = Q.shape[0]  # how many prototypes
 
         # make the matrix sums to 1
