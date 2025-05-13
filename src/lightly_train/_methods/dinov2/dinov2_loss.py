74e41d1aacd9e7cea06c40f8dc338ced13368c07
yutong <yutong@lightly.ai>
2025-05-13 14:16:53 +0200

    add meta license

--- src/lightly_train/_methods/dinov2/dinov2_loss.py
+++ src/lightly_train/_methods/dinov2/dinov2_loss.py
@@ -8,8 +8,8 @@
 
 # Modifications Copyright 2025 Lightly AG:
 # - Import xFormers' cross entropy only if XFORMERS_ENABLED is True
-# - Use dist.is_initialized() to control the all_reduce operation of B in distributed setting in the iBOTPatchLoss's sinkhorn_knopp_teacher
-# - Rename iBOTPatchLoss's to IBOTPatchLoss
+# - Use dist.is_initialized() to control the all_reduce operation of B in distributed setting in the iBOTPatchLoss' sinkhorn_knopp_teacher
+# - Rename iBOTPatchLoss to IBOTPatchLoss
 # - Add type hints to the functions
 
 
