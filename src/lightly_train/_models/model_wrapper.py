6ee594f70e4340830a7568862cec39f736107657
guarin <guarin@lightly.ai>
2025-05-22 16:35:52 +0100

    Improve error message for custom models

--- src/lightly_train/_models/model_wrapper.py
+++ src/lightly_train/_models/model_wrapper.py
@@ -17,6 +17,7 @@ from typing import (
     runtime_checkable,
 )
 
+import typing_extensions
 from torch import Tensor
 from torch.nn import Module, Parameter
 from typing_extensions import NotRequired, Required, TypedDict, TypeVar
@@ -110,3 +111,23 @@ class NNModule(Protocol):
 class ModelWrapper(
     ForwardFeatures, ForwardPool, FeatureDim, ModelGetter, NNModule, Protocol
 ): ...
+
+
+def missing_model_wrapper_attrs(
+    obj: Any, exclude_module_attrs: bool = False
+) -> list[str]:
+    """Returns a list of attributes that are missing in the model wrapper.
+
+    Args:
+        obj:
+            The object to check for missing attributes.
+        exclude_module_attrs:
+            If True, do not check attributes that are also in torch.nn.Module.
+    """
+    missing_attrs = []
+    for attr in typing_extensions.get_protocol_members(ModelWrapper):
+        if exclude_module_attrs and hasattr(Module, attr):
+            continue
+        if not hasattr(obj, attr):
+            missing_attrs.append(attr)
+    return sorted(missing_attrs)
