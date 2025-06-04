42bc000d1246e8f6a726b1acdce84fc894875a88
yutong <yutong@lightly.ai>
2025-06-04 08:59:34 +0200

    improve tests

--- tests/_methods/dinov2/test_utils.py
+++ tests/_methods/dinov2/test_utils.py
@@ -12,65 +12,56 @@ from lightly_train._methods.dinov2.utils import MaskingGenerator, create_collate
 
 
 class TestCreateCollatedMasks:
-    def setup_method(self):
+    def setup_method(self) -> None:
         self.grid_size = 16
         self.masking_generator = MaskingGenerator(
             input_size=(self.grid_size, self.grid_size),
             max_num_patches=int(0.5 * self.grid_size**2),
         )
 
-    def test_create_collated_masks__output_size(self) -> None:
-        num_crops = 4
-
+    @pytest.mark.parametrize("expected_n_crops", [1, 2, 4, 8])
+    def test_create_collated_masks__output_size(self, expected_n_crops: int) -> None:
         masks = create_collated_masks(
             mask_ratio_min=0.1,
             mask_ratio_max=0.5,
-            n_masked_crops=2,
-            n_crops=4,
+            n_masked_crops=min(2, expected_n_crops),
+            n_crops=expected_n_crops,
             mask_generator=self.masking_generator,
         )
 
         collated_masks = masks["collated_masks"]
 
         mask_shape = collated_masks.shape
-        assert mask_shape == (num_crops, self.grid_size**2)
+        assert mask_shape == (expected_n_crops, self.grid_size**2)
 
-    @pytest.mark.parametrize(
-        "num_crops, expected_num_crops_masked",
-        [
-            (1, 0),
-            (1, 1),
-            (4, 2),
-            (4, 3),
-        ],
-    )
-    def test_create_collated_masks__num_crops_masked(
-        self, num_crops: int, expected_num_crops_masked: int
+    @pytest.mark.parametrize("expected_n_masked_crops", [0, 1, 2, 3, 4])
+    def test_create_collated_masks__n_masked_crops(
+        self, expected_n_masked_crops: int
     ) -> None:
         masks = create_collated_masks(
             mask_ratio_min=0.1,
             mask_ratio_max=0.5,
-            n_masked_crops=expected_num_crops_masked,
-            n_crops=num_crops,
+            n_masked_crops=expected_n_masked_crops,
+            n_crops=max(4, expected_n_masked_crops),
             mask_generator=self.masking_generator,
         )
 
         collated_masks = masks["collated_masks"]
 
         num_crops_masked = sum(m.sum() > 0 for m in collated_masks)
-        assert num_crops_masked == expected_num_crops_masked
+        assert num_crops_masked == expected_n_masked_crops
 
     @pytest.mark.parametrize(
         "mask_ratio_min, mask_ratio_max",
         [(0.0, 0.0), (0.1, 0.5), (1.0, 1.0)],
     )
-    def test_create_collated_masks__min_max_image_mask_ratio(
+    def test_create_collated_masks__mask_ratio_min_max(
         self, mask_ratio_min: float, mask_ratio_max: float
     ) -> None:
         masks = create_collated_masks(
             mask_ratio_min=mask_ratio_min,
             mask_ratio_max=mask_ratio_max,
-            n_masked_crops=4,
+            n_masked_crops=2,
             n_crops=4,
             mask_generator=self.masking_generator,
         )
