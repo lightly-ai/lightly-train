4438db36b7685ab4e4832a4215b0f9ec375e6d08
Thomas <thomas@lightly.ai>
2025-07-02 09:09:18 +0200

    Add linear segmentation model.

--- src/lightly_train/_task_models/task_model.py
+++ src/lightly_train/_task_models/task_model.py
@@ -5,7 +5,7 @@
 # This source code is licensed under the license found in the
 # LICENSE file in the root directory of this source tree.
 #
-from torch import Module
+from torch.nn import Module
 
 
 class TaskModel(Module):
