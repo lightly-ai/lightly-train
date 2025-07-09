0bfcb1bc948dfdee95dfac989984b18608e4eb00
yutong <yutong@lightly.ai>
2025-07-09 15:13:50 +0200

    set devicestats to None

--- src/lightly_train/_callbacks/callback_args.py
+++ src/lightly_train/_callbacks/callback_args.py
@@ -32,11 +32,10 @@ class CallbackArgs(PydanticConfig):
     learning_rate_monitor: LearningRateMonitorArgs | None = Field(
         default_factory=LearningRateMonitorArgs
     )
-    device_stats_monitor: DeviceStatsMonitorArgs | None = Field(
-        default_factory=DeviceStatsMonitorArgs
-    )
     early_stopping: EarlyStoppingArgs | None = Field(default_factory=EarlyStoppingArgs)
     model_export: ModelExportArgs | None = Field(default_factory=ModelExportArgs)
     model_checkpoint: ModelCheckpointArgs | None = Field(
         default_factory=ModelCheckpointArgs
     )
+    
+    device_stats_monitor: DeviceStatsMonitorArgs | None = None
