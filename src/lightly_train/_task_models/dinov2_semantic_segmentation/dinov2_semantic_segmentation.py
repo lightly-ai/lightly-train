09a5176a4c3f405539cb7a6f12a328b761d0b0cc
guarin <guarin@lightly.ai>
2025-07-23 15:18:00 +0100

    Enable attn mask in seg forward

--- src/lightly_train/_task_models/dinov2_semantic_segmentation/dinov2_semantic_segmentation.py
+++ src/lightly_train/_task_models/dinov2_semantic_segmentation/dinov2_semantic_segmentation.py
@@ -198,34 +198,32 @@ class DINOv2SemanticSegmentation(TaskModel):
                 mask_logits_per_layer.append(mask_logits)
                 class_logits_per_layer.append(class_logits)
 
-                # NOTE: Changed from original code to only run during training.
-                if self.training:
-                    attn_mask = torch.ones(
-                        x.shape[0],
-                        x.shape[1],
-                        x.shape[1],
-                        dtype=torch.bool,
-                        device=x.device,
-                    )
-                    interpolated = F.interpolate(
-                        input=mask_logits,
-                        size=grid_size,
-                        mode="bilinear",
-                    )
-                    interpolated = interpolated.view(
-                        interpolated.size(0), interpolated.size(1), -1
-                    )
-                    attn_mask[
-                        :,
-                        : self.num_queries,
-                        self.num_queries + 1 + self.backbone.num_register_tokens :,
-                    ] = interpolated > 0
-                    attn_mask = self._disable_attn_mask(
-                        attn_mask=attn_mask,
-                        prob=self.attn_mask_probs[
-                            i - len(self.backbone.blocks) + self.num_joint_blocks
-                        ],
-                    )
+                attn_mask = torch.ones(
+                    x.shape[0],
+                    x.shape[1],
+                    x.shape[1],
+                    dtype=torch.bool,
+                    device=x.device,
+                )
+                interpolated = F.interpolate(
+                    input=mask_logits,
+                    size=grid_size,
+                    mode="bilinear",
+                )
+                interpolated = interpolated.view(
+                    interpolated.size(0), interpolated.size(1), -1
+                )
+                attn_mask[
+                    :,
+                    : self.num_queries,
+                    self.num_queries + 1 + self.backbone.num_register_tokens :,
+                ] = interpolated > 0
+                attn_mask = self._disable_attn_mask(
+                    attn_mask=attn_mask,
+                    prob=self.attn_mask_probs[
+                        i - len(self.backbone.blocks) + self.num_joint_blocks
+                    ],
+                )
 
             # This mirrors forward of DINOv2 Block.
             if self.training and block.sample_drop_ratio > 0:
