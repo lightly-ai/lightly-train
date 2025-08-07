6ec73f4240c7d8f5fe933595bd1fe79404d78f9f
Lionel <lionel@lightly.ai>
2025-08-07 16:47:01 +0200

    support python 3.8

--- src/lightly_train/_methods/method_args.py
+++ src/lightly_train/_methods/method_args.py
@@ -6,6 +6,8 @@
 # LICENSE file in the root directory of this source tree.
 #
 
+from __future__ import annotations
+
 from typing import ClassVar, Literal
 
 from lightly_train._configs.config import PydanticConfig
