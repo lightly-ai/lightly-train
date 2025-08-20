2ccd60b2ac4134d2d8fefb829a9780ff973eee3a
Simon Schoelly <sischoel@gmail.com>
2025-08-20 23:43:49 +0200

    Add license header

--- tests/_commands/test_export_task.py
+++ tests/_commands/test_export_task.py
@@ -1,3 +1,11 @@
+#
+# Copyright (c) Lightly AG and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+#
+
 import sys
 from pathlib import Path
 
