65aa004376397a8faab5e40b7de142b183ce5220
Lionel <lionel@lightly.ai>
2025-09-15 15:56:18 +0200

    fix formatting

--- tests/_transforms/test_random_order.py
+++ tests/_transforms/test_random_order.py
@@ -1,12 +1,24 @@
-from lightly_train._transforms.random_order import RandomOrder
-from albumentations import Resize, ColorJitter
-import pytest
+#
+# Copyright (c) Lightly AG and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+#
 import numpy as np
+import pytest
+from albumentations import ColorJitter, Resize
+
+from lightly_train._transforms.random_order import RandomOrder
+
 
 class TestRandomOrder:
     def test__shapes(self) -> None:
         tr = RandomOrder(
-            transforms=[Resize(32, 32, p=1.0), ColorJitter(brightness=0.5, contrast=0.5)],
+            transforms=[
+                Resize(32, 32, p=1.0),
+                ColorJitter(brightness=0.5, contrast=0.5),
+            ],
             n=2,
             p=1.0,
         )
@@ -17,7 +29,10 @@ class TestRandomOrder:
     @pytest.mark.parametrize("n", [0, 1, 2])
     def test__get_idx(self, n: int) -> None:
         tr = RandomOrder(
-            transforms=[Resize(32, 32, p=1.0), ColorJitter(brightness=0.5, contrast=0.5)],
+            transforms=[
+                Resize(32, 32, p=1.0),
+                ColorJitter(brightness=0.5, contrast=0.5),
+            ],
             n=n,
             p=1.0,
         )
