9102d51b3fd34462c32499c7b76dc9e8ab259f70
guarin <guarin@lightly.ai>
2025-10-21 15:30:27 +0100

    Add __future__ import

--- src/lightly_train/_transforms/instance_segmentation_transform.py
+++ src/lightly_train/_transforms/instance_segmentation_transform.py
@@ -5,6 +5,8 @@
 # This source code is licensed under the license found in the
 # LICENSE file in the root directory of this source tree.
 #
+from __future__ import annotations
+
 from albumentations.pytorch.transforms import ToTensorV2
 from torch import Tensor
 from typing_extensions import Literal
