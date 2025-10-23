81eabc659bbe609956a7e53fbcdbd19814a34a93
Simon Schoelly <sischoel@gmail.com>
2025-10-23 09:07:04 +0200

    Add license header to tests

--- tests/_task_models/test_task_model_helpers.py
+++ tests/_task_models/test_task_model_helpers.py
@@ -1,3 +1,10 @@
+#
+# Copyright (c) Lightly AG and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+#
 import re
 from pathlib import Path
 from unittest.mock import patch
