e26edfcbe7b3c4ecd96ec0d17ce86620e956efd0
yutong <yutong@lightly.ai>
2025-10-24 13:35:06 +0000

    fix bugs

--- src/lightly_train/__init__.py
+++ src/lightly_train/__init__.py
@@ -32,6 +32,7 @@ from lightly_train._commands.common_helpers import ModelFormat, ModelPart
 from lightly_train._commands.embed import embed
 from lightly_train._commands.export import export
 from lightly_train._commands.export_task import export_onnx
+from lightly_train._commands.predict_task import predict_semantic_segmentation
 from lightly_train._commands.train import train
 from lightly_train._commands.train_task import train_semantic_segmentation
 from lightly_train._embedding.embedding_format import EmbeddingFormat
@@ -50,6 +51,7 @@ __all__ = [
     "ModelPart",
     "train",
     "train_semantic_segmentation",
+    "predict_semantic_segmentation",
     "load_model_from_checkpoint",
 ]
 
