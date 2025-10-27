613399c75cbaf249a250e975c954dcc2e2d71611
Lionel <lionel.peer@gmail.com>
2025-10-27 15:35:06 +0100

    turn into module

--- src/lightly_train/_task_models/object_detection_components/ema.py
+++ src/lightly_train/_task_models/object_detection_components/ema.py
@@ -17,9 +17,10 @@ from copy import deepcopy
 
 import torch
 import torch.nn as nn
+from torch.nn import Module
 
 
-class ModelEMA(object):
+class ModelEMA(Module):
     """
     Model Exponential Moving Average from https://github.com/rwightman/pytorch-image-models
     Keep a moving average of everything in the model state_dict (parameters and buffers).
@@ -62,20 +63,6 @@ class ModelEMA(object):
                 if v.dtype.is_floating_point:
                     v *= d
                     v += (1 - d) * msd[k].detach()
-
-    def to(self, *args, **kwargs):
-        self.module = self.module.to(*args, **kwargs)
-        return self
-
-    def state_dict(
-        self,
-    ):
-        return dict(module=self.module.state_dict(), updates=self.updates)
-
-    def load_state_dict(self, state, strict=True):
-        self.module.load_state_dict(state["module"], strict=strict)
-        if "updates" in state:
-            self.updates = state["updates"]
 
     def forwad(
         self,
