b98ce6a87e298beffc0a841ab879b05359835523
thomas <thomas@lightly.ai>
2025-10-28 08:00:07 +0100

    Add map metrics to dinov2 lt-detr

--- src/lightly_train/_task_models/dinov2_ltdetr_object_detection/train_model.py
+++ src/lightly_train/_task_models/dinov2_ltdetr_object_detection/train_model.py
@@ -46,8 +46,8 @@ from lightly_train.types import ObjectDetectionBatch, PathLike
 
 
 class DINOv2LTDETRObjectDetectionTaskSaveCheckpointArgs(TaskSaveCheckpointArgs):
-    watch_metric: str = "val_total_loss"
-    mode: Literal["min", "max"] = "min"
+    watch_metric: str = "val_metric/map"
+    mode: Literal["min", "max"] = "max"
 
 
 class DINOv2LTDETRObjectDetectionTrainModelArgs(TrainModelArgs):
@@ -119,6 +119,9 @@ class DINOv2LTDETRObjectDetectionTrain(TrainModel):
         val_transform_args: DINOv2LTDETRObjectDetectionValTransformArgs,
     ) -> None:
         super().__init__()
+        # Lazy import torchmetrics because it is an optional dependency.
+        from torchmetrics.detection.mean_ap import MeanAveragePrecision
+
         self.model_args = model_args
         self.model = DINOv2LTDETRObjectDetection(
             model_name=model_name,
@@ -145,6 +148,9 @@ class DINOv2LTDETRObjectDetectionTrain(TrainModel):
         )
 
         self.clip_max_norm = model_args.clip_max_norm
+
+        # Validation metric.
+        self.map_metric = MeanAveragePrecision()
 
     def set_train_mode(self) -> None:
         super().set_train_mode()
@@ -179,7 +185,7 @@ class DINOv2LTDETRObjectDetectionTrain(TrainModel):
 
         return TaskStepResult(
             loss=total_loss,
-            log_dict={**{"train_total_loss": total_loss.item()}, **loss_dict},
+            log_dict={**{"train_loss": total_loss.item()}, **loss_dict},
         )
 
     def validation_step(
@@ -213,9 +219,28 @@ class DINOv2LTDETRObjectDetectionTrain(TrainModel):
         # Average loss dict across devices.
         loss_dict = reduce_dict(loss_dict)
 
+        # Convert model outputs into torchmetrics-compatible format
+        preds = []
+        for bboxes, logits in zip(outputs["pred_boxes"], outputs["pred_logits"]):
+            scores = logits.softmax(dim=-1).max(dim=-1)
+            preds.append(
+                {
+                    "boxes": bboxes,  # [N, 4] in xyxy
+                    "scores": scores.values,  # [N]
+                    "labels": scores.indices,  # [N]
+                }
+            )
+
+        # Update mAP metric
+        self.map_metric.update(preds, targets)
+
+        metrics: dict[str, Any] = {
+            "val_metric/": self.map_metric,
+        }
+
         return TaskStepResult(
             loss=total_loss,
-            log_dict={**{"val_total_loss": total_loss.item()}, **loss_dict},
+            log_dict={**{"val_loss": total_loss.item()}, **loss_dict, **metrics},
         )
 
     def get_optimizer(self, total_steps: int) -> tuple[Optimizer, LRScheduler]:
