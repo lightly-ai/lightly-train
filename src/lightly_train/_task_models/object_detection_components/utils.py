72c43f17bd9fc19d2e58cda31a8fe3847cda45dd
thomas <thomas@lightly.ai>
2025-11-03 08:33:48 +0100

    Update notice file

--- src/lightly_train/_task_models/object_detection_components/utils.py
+++ src/lightly_train/_task_models/object_detection_components/utils.py
@@ -10,7 +10,11 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.#
-"""Copyright(c) 2023 lyuwenyu. All Rights Reserved."""
+"""Copyright(c) 2023 lyuwenyu. All Rights Reserved.
+
+# Modifications Copyright 2025 Lightly AG:
+- Added _yolo_to_xyxy and _denormalize_xyxy_boxes functions
+"""
 
 from __future__ import annotations
 
