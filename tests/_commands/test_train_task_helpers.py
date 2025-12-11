b7ed7f5ae69c737d12a852bdddab8782d0864fbd
guarin <guarin@lightly.ai>
2025-12-11 09:07:47 +0000

    Improve test

--- tests/_commands/test_train_task_helpers.py
+++ tests/_commands/test_train_task_helpers.py
@@ -5,6 +5,9 @@
 # This source code is licensed under the license found in the
 # LICENSE file in the root directory of this source tree.
 #
+from __future__ import annotations
+
+import os
 from pathlib import Path
 
 import torch
@@ -22,6 +25,9 @@ def test__torch_weights_only_false(tmp_path: Path) -> None:
     ckpt = {"dummy": DummyClass()}
     ckpt_path = tmp_path / "model.ckpt"
     fabric.save(ckpt_path, ckpt)  # type: ignore
+    assert os.environ.get("TORCH_FORCE_NO_WEIGHTS_ONLY_LOAD") is None
     with train_task_helpers._torch_weights_only_false():
+        assert os.environ.get("TORCH_FORCE_NO_WEIGHTS_ONLY_LOAD") == "1"
         torch.load(ckpt_path)
         fabric.load(ckpt_path)
+    assert os.environ.get("TORCH_FORCE_NO_WEIGHTS_ONLY_LOAD") is None
