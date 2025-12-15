9f47f408d8793c66b7ce6f0c2804dc9f416960c4
Thomas <thomas@lightly.ai>
2025-12-15 16:24:57 +0000

    add sahi  to dinov2 od

--- src/lightly_train/_task_models/dinov2_ltdetr_object_detection/task_model.py
+++ src/lightly_train/_task_models/dinov2_ltdetr_object_detection/task_model.py
@@ -24,6 +24,7 @@ from lightly_train._models.dinov2_vit.dinov2_vit_package import DINOV2_VIT_PACKA
 from lightly_train._task_models.dinov2_ltdetr_object_detection.dinov2_vit_wrapper import (
     DINOv2ViTWrapper,
 )
+from lightly_train._task_models.object_detection_components import tiling_utils
 from lightly_train._task_models.object_detection_components.hybrid_encoder import (
     HybridEncoder,
 )
@@ -241,7 +242,7 @@ class DINOv2LTDETRObjectDetection(TaskModel):
 
         h, w = x.shape[-2:]
 
-        x = transforms_functional.to_dtype(x, scale=True, dtype=torch.float32)
+        x = transforms_functional.to_dtype(x, dtype=torch.float32, scale=True)
 
         # Normalize the image.
         if self.image_normalize is not None:
@@ -251,13 +252,129 @@ class DINOv2LTDETRObjectDetection(TaskModel):
         x = transforms_functional.resize(x, self.image_size)
         x = x.unsqueeze(0)
 
-        labels, boxes, scores = self(x, orig_target_size=(h, w))
+        labels, boxes, scores = self(x, orig_target_size=torch.tensor([[h, w]]))
         keep = scores > threshold
         labels, boxes, scores = labels[keep], boxes[keep], scores[keep]
         return {
-            "labels": labels.squeeze(0),
-            "bboxes": boxes.squeeze(0),
-            "scores": scores.squeeze(0),
+            "labels": labels,
+            "bboxes": boxes,
+            "scores": scores,
+        }
+
+    @torch.no_grad()
+    def predict_tiled(
+        self,
+        image: PathLike | PILImage | Tensor,
+        threshold: float = 0.6,
+        overlap: float = 0.2,
+        nms_iou_threshold: float = 0.3,
+        global_local_iou_threshold: float = 0.1,
+    ) -> dict[str, Tensor]:
+        """
+        Run tiled inference on the input image and merge predictions.
+
+        The image is first converted to a tensor, then:
+        - Tiled into overlapping crops of size `self.image_size`.
+        - A resized full-image version is added as a "global" tile.
+        - All tiles (global + local) are passed through the model in parallel.
+        - Predictions are filtered by score and merged using NMS and a
+          global/local consistency heuristic. NMS is only applied on tiles predictions.
+          The heuristic discards tiles predictions that heavily overlaps with global
+          predictions.
+
+        Args:
+            image: Input image. Can be a path, a PIL image, or a tensor of shape (C, H, W).
+            threshold: Score threshold for filtering low-confidence predictions.
+            overlap: Fractional overlap between tiles in [0, 1). 0.0 means no overlap.
+            nms_iou_threshold: IoU threshold used for non-maximum suppression when merging
+                predictions from tiles and global image. A lower nms_iou_threshold
+                value yields less predictions.
+            global_local_iou_threshold: Minimum IoU required to consider a tile prediction
+                as matching a global prediction when combining them. A lower
+                global_local_iou_threshold yields less predictions.
+
+        Returns:
+            dict[str, Tensor]: A dictionary with:
+                - "labels": Tensor of shape (N,) with predicted class indices.
+                - "bboxes": Tensor of shape (N, 4) with bounding boxes in (x_min, y_min, x_max, y_max)
+                  in the coordinates of the original image.
+                - "scores": Tensor of shape (N,) with confidence scores for each prediction.
+        """
+
+        if self.training or not self.postprocessor.deploy_mode:
+            self.deploy()
+
+        device = next(self.parameters()).device
+        x = file_helpers.as_image_tensor(image).to(device)
+
+        # Tile the image.
+        tiles, tiles_coordinates = tiling_utils._tile_image(x, overlap, self.image_size)
+
+        # Prepare the full image tile
+        h, w = x.shape[-2:]
+        x = transforms_functional.resize(x, self.image_size)
+        x = x.unsqueeze(0)
+        tiles = torch.cat([x, tiles], dim=0)
+
+        # Normalize the tiles and the image together.
+        tiles = transforms_functional.to_dtype(tiles, dtype=torch.float32, scale=True)
+
+        # Normalize the image.
+        if self.image_normalize is not None:
+            tiles = transforms_functional.normalize(
+                tiles,
+                mean=self.image_normalize["mean"],
+                std=self.image_normalize["std"],
+            )
+
+        # Prepare the image/tiles sizes.
+        orig_target_sizes = torch.tensor([self.image_size], device=device).repeat(
+            len(tiles), 1
+        )
+        orig_target_sizes[0, 0] = h
+        orig_target_sizes[0, 1] = w
+
+        # Feed the tiles in parallel to the model.
+        labels, boxes, scores = self(tiles, orig_target_size=orig_target_sizes)
+
+        # Add coordinates of the tiles to the boxes.
+        tiles_coordinates = (
+            tiles_coordinates.repeat(1, 2).unsqueeze(1).expand(-1, boxes.shape[1], -1)
+        )
+        boxes[1:] += tiles_coordinates
+
+        # Reorganize the predictions.
+        boxes_global = boxes[0].view(-1, 4)
+        boxes_tiles = boxes[1:].view(-1, 4)
+        labels_global = labels[0].flatten()
+        labels_tiles = labels[1:].flatten()
+        scores_global = scores[0].flatten()
+        scores_tiles = scores[1:].flatten()
+
+        # Discard low-confidence predictions.
+        keep_global = scores_global > threshold
+        keep_tiles = scores_tiles > threshold
+
+        # Combine global and tiles predictions.
+        labels, boxes, scores = tiling_utils._combine_predictions_tiles_and_global(
+            pred_global={
+                "labels": labels_global[keep_global],
+                "bboxes": boxes_global[keep_global],
+                "scores": scores_global[keep_global],
+            },
+            pred_tiles={
+                "labels": labels_tiles[keep_tiles],
+                "bboxes": boxes_tiles[keep_tiles],
+                "scores": scores_tiles[keep_tiles],
+            },
+            nms_iou_threshold=nms_iou_threshold,
+            global_local_iou_threshold=global_local_iou_threshold,
+        )
+
+        return {
+            "labels": labels,
+            "bboxes": boxes,
+            "scores": scores,
         }
 
     def deploy(self) -> Self:
@@ -275,16 +392,20 @@ class DINOv2LTDETRObjectDetection(TaskModel):
         return x
 
     def forward(
-        self, x: Tensor, orig_target_size: tuple[int, int] | None = None
+        self, x: Tensor, orig_target_size: Tensor | None = None
     ) -> tuple[Tensor, Tensor, Tensor]:
         # Function used for ONNX export
-        h, w = x.shape[-2:]
         if orig_target_size is None:
-            orig_target_size_ = torch.tensor([w, h])[None].to(x.device)
+            h, w = x.shape[-2:]
+            orig_target_size_ = torch.tensor([[w, h]]).to(x.device)
         else:
-            orig_target_size_ = torch.tensor(
-                [orig_target_size[1], orig_target_size[0]]
-            )[None].to(x.device)
+            # Flip from (H, W) to (W, H).
+            orig_target_size = orig_target_size[:, [1, 0]]
+
+            # Move to device.
+            orig_target_size_ = orig_target_size.to(device=x.device, dtype=torch.int64)
+
+        # Forward the image through the model.
         x = self.backbone(x)
         x = self.encoder(x)
         x = self.decoder(x)
