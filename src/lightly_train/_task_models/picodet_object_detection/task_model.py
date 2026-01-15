294b68f64aa0b913ed5376bf7e41d5bf4687e274
IgorSusmelj <igor_7779@hotmail.com>
2026-01-15 14:36:39 +0100

    Fix onnx eport due to onnxslim issue

--- src/lightly_train/_task_models/picodet_object_detection/task_model.py
+++ src/lightly_train/_task_models/picodet_object_detection/task_model.py
@@ -458,8 +458,14 @@ class PicoDetObjectDetection(TaskModel):
             )
 
             session = ort.InferenceSession(out)
+            input_name = session.get_inputs()[0].name
+            if input_name != "images":
+                logger.warning(
+                    'ONNX input name "%s" does not match expected "images".',
+                    input_name,
+                )
             input_feed = {
-                "images": dummy_input.cpu().numpy(),
+                input_name: dummy_input.cpu().numpy(),
             }
             outputs_onnx = session.run(output_names=None, input_feed=input_feed)
             outputs_onnx = tuple(torch.from_numpy(y) for y in outputs_onnx)
