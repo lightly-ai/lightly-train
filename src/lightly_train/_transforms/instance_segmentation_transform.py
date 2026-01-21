4fc5609d34aa4d25448880ae485bb05bbd29455d
guarin <43336610+guarin@users.noreply.github.com>
2026-01-21 14:13:09 +0000

    Add random rotation fine-tune transforms (#577)
    * Add random rotation transform
    * Add random rotation 90 transform

--- src/lightly_train/_transforms/instance_segmentation_transform.py
+++ src/lightly_train/_transforms/instance_segmentation_transform.py
@@ -20,7 +20,9 @@ from albumentations import (
     HorizontalFlip,
     OneOf,
     RandomCrop,
+    RandomRotate90,
     Resize,
+    Rotate,
     SmallestMaxSize,
     VerticalFlip,
 )
@@ -43,6 +45,8 @@ from lightly_train._transforms.transform import (
     NormalizeArgs,
     RandomCropArgs,
     RandomFlipArgs,
+    RandomRotate90Args,
+    RandomRotationArgs,
     ScaleJitterArgs,
     SmallestMaxSizeArgs,
 )
@@ -77,6 +81,8 @@ class InstanceSegmentationTransformArgs(TaskTransformArgs):
     num_channels: int | Literal["auto"]
     normalize: NormalizeArgs | Literal["auto"]
     random_flip: RandomFlipArgs | None
+    random_rotate_90: RandomRotate90Args | None
+    random_rotate: RandomRotationArgs | None
     color_jitter: ColorJitterArgs | None
     # TODO: Lionel(09/25): These are currently not fully used.
     scale_jitter: ScaleJitterArgs | None
@@ -207,6 +213,18 @@ class InstanceSegmentationTransform(TaskTransform):
                 ]
             if transform_args.random_flip.vertical_prob > 0.0:
                 transform += [VerticalFlip(p=transform_args.random_flip.vertical_prob)]
+
+        if transform_args.random_rotate_90 is not None:
+            transform += [RandomRotate90(p=transform_args.random_rotate_90.prob)]
+
+        if transform_args.random_rotate is not None:
+            transform += [
+                Rotate(
+                    limit=transform_args.random_rotate.degrees,
+                    interpolation=transform_args.random_rotate.interpolation,
+                    p=transform_args.random_rotate.prob,
+                )
+            ]
 
         # Optionally apply color jitter.
         if transform_args.color_jitter is not None:
