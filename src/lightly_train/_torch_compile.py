5ce173c5a4eed22d94dd24bb9e900b53ac46d837
guarin <guarin@lightly.ai>
2026-03-09 14:09:57 +0100

    Add info on how to enable torch.compile

--- src/lightly_train/_torch_compile.py
+++ src/lightly_train/_torch_compile.py
@@ -37,7 +37,27 @@ def disable_compile(fn: _T, recursive: bool = True) -> _T:
     return fn
 
 
+# ## How to enable torch.compile for a model
+#
+# 1. Create a <ModelName>TorchCompileArgs class that inherits from TorchCompileArgs
+#    in the train_model.py file
+# 2. Set disable: bool = False in the new class
+# 3. Set TrainModel.torch_compile_args_cls to the new class
+# 4. Implement TrainModel.forward as this is the only method that will be compiled
+#
+# Compilation is disabled by default as most models do not yet support it.
+#
+# training_step and validation_step are not compiled as they contain a lot of logic that
+# is not supported by torch.compile. For example, most batches contain the image
+# filenames which triggers a recompilation for every new filename. Metrics don't support
+# compilation either and are also called inside the train/val steps. Therefore it is
+# easier to move all the compilable code to the forward method and call it there.
+
+
 class TorchCompileArgs(PydanticConfig):
+    # Disable torch.compile for all models by default as most models do not support
+    # compilation out of the box.
+    # See ImageClassificationTrainModel on how to enable it for a specific model.
     disable: bool = True
 
     # Allow extra fields as torch.compile accepts many arguments
@@ -45,6 +65,8 @@ class TorchCompileArgs(PydanticConfig):
 
 
 def try_compile(fn: _T, name: str, torch_compile_args: TorchCompileArgs) -> _T:
+    """Tries to compile a torch model or function and falls back to the uncompiled
+    version if any error occurs."""
     if torch_compile_args.disable:
         return fn
     if not hasattr(torch, "compile"):
