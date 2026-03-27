1e865ceb3a493a16dfe0665482f1219c924cb2ce
Simon Schoelly <sischoel@gmail.com>
2026-03-28 00:04:11 +0100

    Parse labels in YoloObjectDetection.list_image_info

--- tests/_data/test_file_helpers.py
+++ tests/_data/test_file_helpers.py
@@ -460,28 +460,26 @@ def test_open_yolo_oriented_object_detection_label_numpy__empty(
     assert class_labels.shape == (0,)
 
 
-def test_open_yolo_object_detection_label_numpy(tmp_path: Path) -> None:
+def test_open_yolo_object_detection_label(tmp_path: Path) -> None:
     with open(tmp_path / "label.txt", "w") as f:
         f.write("2 0.1 0.1 0.2 0.2\n1 0.3 0.3 0.4 0.4\n")
-    bboxes, class_labels = file_helpers.open_yolo_object_detection_label_numpy(
+    bboxes, class_labels = file_helpers.open_yolo_object_detection_label(
         label_path=tmp_path / "label.txt"
     )
-    np.testing.assert_array_almost_equal(
-        bboxes, np.array([[0.1, 0.1, 0.2, 0.2], [0.3, 0.3, 0.4, 0.4]])
-    )
-    np.testing.assert_array_equal(class_labels, np.array([2, 1]))
+    assert bboxes == [[0.1, 0.1, 0.2, 0.2], [0.3, 0.3, 0.4, 0.4]]
+    assert class_labels == [2, 1]
 
 
-def test_open_yolo_object_detection_label_numpy__empty(
+def test_open_yolo_object_detection_label__empty(
     tmp_path: Path,
 ) -> None:
     with open(tmp_path / "label.txt", "w") as f:
         f.write("")
-    bboxes, class_labels = file_helpers.open_yolo_object_detection_label_numpy(
+    bboxes, class_labels = file_helpers.open_yolo_object_detection_label(
         label_path=tmp_path / "label.txt"
     )
-    assert bboxes.shape == (0, 4)
-    assert class_labels.shape == (0,)
+    assert bboxes == []
+    assert class_labels == []
 
 
 def test_open_yolo_instance_segmentation_label_numpy(tmp_path: Path) -> None:
