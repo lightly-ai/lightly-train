fb3c0d4c3140938c213d3fb6b06bbcd682dce3c8
gabrielfruet <gabrielfruet538@gmail.com>
2026-05-15 14:42:32 +0200

    ltdetr geometry format

--- tests/_task_models/object_detection_components/test_ltdetr_geometry.py
+++ tests/_task_models/object_detection_components/test_ltdetr_geometry.py
@@ -1,3 +1,10 @@
+#
+# Copyright (c) Lightly AG and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+#
 from __future__ import annotations
 
 from lightly_train._task_models.object_detection_components.ltdetr_geometry import (
