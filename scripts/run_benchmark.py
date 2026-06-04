cae56616270c9b723905e4ee416ffc74c00c2b0d
Simon Schoelly <sischoel@gmail.com>
2026-06-04 13:33:31 +0200

    commit

--- scripts/run_benchmark.py
+++ scripts/run_benchmark.py
@@ -1,39 +1,46 @@
 import lightly_train
 
-model = lightly_train.load_model("exported_best.pt", device="cpu")
+if __name__ == "__main__":
+    # model = lightly_train.load_model("exported_best.pt", device="cpu")
+    # model = lightly_train.load_model("dinov3/vitt16-ltdetr-coco")
+    # model = lightly_train.load_model("picodet-s-coco") # does not work atm
+    model = lightly_train.load_model(
+        "dinov3/convnext-tiny-ltdetr-coco"
+    )  # does not work atm
 
-structured_benchmark_results = lightly_train.benchmark_object_detection(
-    out="my_eval_run/eval_run0",
-    data={
-        "format": "coco",
-        "train": {
-            "annotations": "/home/simon/lighlty/lightly-train/datasets/coco-2017/annotations/instances_train2017.json",
-            "images": "/home/simon/lighlty/lightly-train/datasets/coco-2017/val2017",
+    structured_benchmark_results = lightly_train.benchmark_object_detection(
+        out="my_eval_run/eval_run0",
+        data={
+            "format": "coco",
+            "train": {
+                "annotations": "/home/simon/lighlty/lightly-train/datasets/coco-2017/annotations/instances_train2017.json",
+                "images": "/home/simon/lighlty/lightly-train/datasets/coco-2017/val2017",
+            },
+            "val": {
+                "annotations": "/home/simon/lighlty/lightly-train/datasets/coco-2017/annotations/instances_val2017.json",
+                "images": "/home/simon/lighlty/lightly-train/datasets/coco-2017/val2017",
+            },
         },
-        "val": {
-            "annotations": "/home/simon/lighlty/lightly-train/datasets/coco-2017/annotations/instances_val2017.json",
-            "images": "/home/simon/lighlty/lightly-train/datasets/coco-2017/val2017"
+        model=model,
+        num_workers=0,
+        # backend_args={"format": "tensorrt", "precision": "fp16", "export_args": {"onnx_args": {"dynamic_batch_size": False}}},
+        # backend_args={
+        #    "format": "onnx",
+        #    "provider": "tensorrt",
+        #    "export_args": {"dynamic_batch_size": False},
+        # },
+        backend_args={"format": "torch", "compile": True},
+        # backend_args={"format": "onnx", "provider": "cuda", "precision": "fp16", "export_args": {"dynamic_batch_size": False}},
+        # backend_args={"format": "onnx", "provider": "tensorrt", "precision": "fp16", "export_args": {"dynamic_batch_size": False}},
+        # backend_args={"format": "onnx", "provider": "cpu", "export_args": {"dynamic_batch_size": False}},
+        # device="cpu",
+        debug=True,
+        batch_size=32,
+        # steps=100,
+        warmup_steps=0,
+        overwrite=True,
+        metric_args={
+            "detection_threshold": 0.6,
+            "top_k": 3,
         },
-    },
-    model=model,
-    num_workers=10,
-    #backend_args={
-    #    "format": "tensorrt",
-    #},
-    #backend_args={
-    #    "format": "onnx",
-    #    "provider": "tensorrt",
-    #    "export_args": {},  # forwarded to model.export_onnx
-    #},
-    backend_args={"format": "torch"},
-    debug=True,
-    #backend_args={"format": "onnx"},
-    batch_size=8,
-    #steps=4,
-    warmup_steps=0,
-    overwrite=True,
-    metric_args={
-        "detection_threshold": 0.6,
-        "top_k": 3,
-    },
-)
+    )
